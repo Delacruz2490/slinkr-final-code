939bf6a704ce9f96f8807ab80d52ed1c68e07232
Mark Rafael <mrkrflstfu14@gmail.com>
2025-04-25 21:26:27 -0700

    Update Firebase config with real values

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,16 @@
-// Dummy Firebase Config — Replace with real config later
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE",
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuToGIyjxE4hBR7eFx4cXoKcoCAV_kIaU",
+  authDomain: "va-tool.firebaseapp.com",
+  projectId: "va-tool",
+  storageBucket: "va-tool.appspot.com",
+  messagingSenderId: "848303669290",
+  appId: "1:848303669290:web:4b04817e991a50463d0353",
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
